9383fd5b4f251a4b16f9fd73b74423812afa081f
Marta Diez Asensio <mdiez-as@car11s1.42barcelona.com>
2024-02-07 19:15:37 +0100

    exercises ex04 and ex03 done

--- practice/exercises/CPP01/ex03/HumanA.cpp
+++ practice/exercises/CPP01/ex03/HumanA.cpp
@@ -1,7 +1,7 @@
 
 #include "HumanA.hpp"
 
-HumanA::HumanA(std::string name, Weapon &weapon): _name(name), _weapon(weapon)
+HumanA::HumanA(std::string name, Weapon &weapon): _weapon(weapon), _name(name)
 {
 	std::cout << "HumanA " << name << " created with ";
 	std::cout << weapon.getType() << std::endl;
--- practice/exercises/CPP01/ex03/HumanB.cpp
+++ practice/exercises/CPP01/ex03/HumanB.cpp
@@ -1,6 +1,6 @@
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name): _name(name), _weapon(NULL)
+HumanB::HumanB(std::string name): _weapon(NULL), _name(name)
 {
 	std::cout << "HumanB " << name << " created with no weapon" << std::endl;
 }
@@ -10,7 +10,7 @@ HumanB::~HumanB( void ) {
 }
 
 void	HumanB::attack( void ) {
-	std::cout << this->_name << "attacks with his" << this->_weapon.getType() << std::endl;
+	std::cout << this->_name << "attacks with his" << this->_weapon->getType() << std::endl;
 }
 
 void	HumanB::setWeapon(Weapon &weapon) {
